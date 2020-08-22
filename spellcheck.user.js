d61ad742b3ab50644119046c846083003201c74a
Jeremy Bowers <jerf@jerf.org>
2020-08-22 16:59:11 -0400

    Create spellcheck.user.js
    This userscript will remove the "spellcheck='false'" attribute, recursing through iframes.

---
+++ spellcheck.user.js
@@ -0,0 +1,22 @@
+// ==UserScript==
+// @name        Kill Spellcheck w/ Recursive iframe Traversal
+// @description Recursively remove spellcheck attributes through iframes.
+// @author      Jeremy Bowers
+// @namespace   jerf
+// @version     1.0
+// ==/UserScript==
+
+function crawlIframes(node) {
+  node.contentDocument.querySelectorAll("iframe").forEach(crawlIframes);
+  
+  node.contentDocument.querySelectorAll('[spellcheck="false"]').forEach((spellNode) => {
+    console.log("spellcheck removed");
+    spellNode.removeAttribute("spellcheck")
+  });
+}
+
+function removeSpellcheckRestrictions() {
+  document.querySelectorAll("iframe").forEach(f => crawlIframes(f));
+}
+
+window.setInterval(removeSpellcheckRestrictions, 1000);
